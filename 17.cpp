d09b5f9e5d78da08132bff4e25da9a296b76e096
menphim <tmkppr.0208@gmail.com>
2016-08-31 00:34:34 +0900

    17(作りかけ？)

---
+++ 17.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+	//one,two,three,four,five,six,seven,eight,nine,ten,eleven,twelve,thirteen,fourteen...twenty..thirty..fourty..fifty..sixty..seventy..eighty..ninety..one hundred..thousand
+	int tmp,c=0;
+	for(int i=1;i<=1000;i++){
+		tmp=i%100;
+		if(tmp>=10&&tmp<20){
+			
+		}
+		if(tmp==1&&tmp==2&&tmp==6) c+=3;
+		else if(tmp==1&&tmp==2&&tmp==6) c+=3;
+	}
+}
